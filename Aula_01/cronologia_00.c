0f0e6975310ef3d90161ee4f479d1c651ce06425
etvorellana <evalero@uesc.br>
2025-03-10 13:08:13 -0300

    Exemplos de revisão de C

---
+++ Aula_01/cronologia_00.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+    Queremos er uma serie de datas, verificar se todas elas 
+    são representam datas válidas e imprimir elas em ordem 
+    cronológica.
+*/
+
+int main(void){
+    // Como fazer?
+    return 0;
+} // Fim da função main
+    
