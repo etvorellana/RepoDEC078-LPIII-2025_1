f3cff8a2b0267f9cf183b011f29fc0f9f69fb1ba
etvorellana <evalero@uesc.br>
2025-03-28 09:22:38 -0300

    Aula 5

--- Aula_04/Main_03.java
+++ Aula_04/Main_03.java
@@ -1,12 +1,14 @@
-public class Main_02 {
+public class Main_03 {
     public static void main(String[] args) {
-        JogoDadosPrivate jogo = new JogoDadosPrivate();
-        jogo.inicializa(10, 1, 6, 0);
+        JogoDados jogo = new JogoDados(10, 1, 6, 1234);
+        //jogo.inicializa(10, 1, 6, 0);
         jogo.jogaOsDados();
         jogo.printJogo();
         //jogo.qtVezes = 5;
+        jogo.jogaOsDados();
         jogo.printJogo();
         //jogo.qtVezes = 15;
+        jogo.jogaOsDados();
         jogo.printJogo();
 
     }
